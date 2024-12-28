fde285e3549e786e419d994a2e688631785a961d
尤雨东 <root@ltpp.vip>
2024-12-28 20:59:45 +0800

    feat:mut remove

--- src/generate/constant.rs
+++ src/generate/constant.rs
@@ -1,5 +1,4 @@
 pub const FIELD_SHOULD_HAVE_A_NAME: &str = "Field should have a name";
 pub const GET_METHOD_PREFIX: &str = "get_";
-pub const GET_MUT_METHOD_PREFIX: &str = "get_mut_";
 pub const SET_METHOD_PREFIX: &str = "set_";
 pub const UNSUPPORTED_LOMBOK_DERIVE: &str = "#[derive(Lombok)] is only supported for structs.";
