7301a219b26ddc6afb9e882327d2ec228ac0acf7
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:36 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/lombok-macros.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/lombok-macros.git;
 git remote add origin git@github.com:ltpp-universe/lombok-macros.git;
