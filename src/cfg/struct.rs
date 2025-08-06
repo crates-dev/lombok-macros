50147c742c371f9ef66014603448f3ce39f78491
尤雨东 <root@ltpp.vip>
2025-08-06 18:45:32 +0800

    feat: v1.13.11

--- src/cfg/struct.rs
+++ src/cfg/struct.rs
@@ -6,15 +6,15 @@ use crate::{func::*, visibility::*};
 /// whether to skip processing (`skip`), and the visibility of the function (`Visibility`).
 ///
 /// # Fields
-/// - `func_type`: A `FuncType` that specifies the function type (e.g., `Get`, `Set`, or `Unknown`).
+/// - `func_type`: A `FuncType` that specifies the function type.
 /// - `skip`: A boolean flag indicating whether the function should be skipped during processing.
-/// - `visibility`: A `Visibility` that defines the visibility of the function (e.g., `Public`, `Private`).
+/// - `visibility`: A `Visibility` that defines the visibility of the function.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Cfg {
-    /// A `FuncType` that specifies the function type (e.g., `Get`, `Set`, or `Unknown`).
+    /// A `FuncType` that specifies the function type.
     pub(crate) func_type: FuncType,
     /// A boolean flag indicating whether the function should be skipped during processing.
     pub(crate) skip: bool,
-    /// A `Visibility` that defines the visibility of the function (e.g., `Public`, `Private`).
+    /// A `Visibility` that defines the visibility of the function.
     pub(crate) visibility: Visibility,
 }
