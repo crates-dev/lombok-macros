d3637cf4e71ad1b522e45385a77a0d9238c68714
尤雨东 <root@ltpp.vip>
2026-01-17 15:54:50 +0800

    feat: v1.15.7

--- src/generate/const.rs
+++ src/generate/const.rs
@@ -1,6 +1,15 @@
 /// Error message indicating that a field must have a name.
 pub(crate) const FIELD_SHOULD_HAVE_A_NAME: &str = "Field should have a name";
 
+/// Error message indicating that #[derive(Data)] is only supported for structs.
+pub(crate) const UNSUPPORTED_DATA_DERIVE: &str = "#[derive(Data)] is only supported for structs.";
+
+/// Error message indicating that #[derive(New)] is only supported for structs.
+pub(crate) const UNSUPPORTED_NEW_DERIVE: &str = "#[derive(New)] is only supported for structs.";
+
+/// The Option type identifier.
+pub(crate) const OPTION_TYPE: &str = "Option";
+
 /// Prefix for getter methods.
 pub(crate) const GET_METHOD_PREFIX: &str = "get_";
 
@@ -10,17 +19,8 @@ pub(crate) const GET_MUT_METHOD_PREFIX: &str = "get_mut_";
 /// Prefix for setter methods.
 pub(crate) const SET_METHOD_PREFIX: &str = "set_";
 
-/// Error message indicating that #[derive(Data)] is only supported for structs.
-pub(crate) const UNSUPPORTED_DATA_DERIVE: &str = "#[derive(Data)] is only supported for structs.";
-
-/// Error message indicating that #[derive(New)] is only supported for structs.
-pub(crate) const UNSUPPORTED_NEW_DERIVE: &str = "#[derive(New)] is only supported for structs.";
-
 /// The string prefix used for raw identifiers.
 pub(crate) const RAW_IDENT_PREFIX: &str = "r#";
-
-/// The Option type identifier.
-pub(crate) const OPTION_TYPE: &str = "Option";
 
 /// The try_get method prefix.
 pub(crate) const TRY_GET_METHOD_PREFIX: &str = "try_";
